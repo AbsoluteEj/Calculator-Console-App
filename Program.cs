e3d3685a2f425c154ce5da5662d1f8d348dee15b
AbsoluteEj <ejsadiarin@gmail.com>
2022-08-30 14:25:45 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the Calculator Console App");
+        }
+    }
+}
